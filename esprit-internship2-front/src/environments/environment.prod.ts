c32782fd6b1d9df0183949d57e52ca09baaeabf0
MDJ-Github <54294124+MDJ-GitHub@users.noreply.github.com>
2024-10-26 00:58:29 +0100

    v1.4 - Ready for deployment

--- esprit-internship2-front/src/environments/environment.prod.ts
+++ esprit-internship2-front/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://20.220.23.6:30001/esprit-internship2-back',
+  apiUrl: 'http://4.206.216.97:30001/esprit-internship2-back',
   appName: 'SNCFT - Gestion des Frets'
 };
